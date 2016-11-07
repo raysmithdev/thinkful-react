532df1c224cee8e429ed004d27886fb8a46c2e3a
Alvin Kato <Alvin@Alvins-MacBook-Pro.local>
2016-11-07 23:32:39 +0300

    Adds the DOM render function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 require('babel-polyfill');
 
+var ReactDom = require('react-dom');
+var Person = require('./components/person');
+
+document.addEventListener('DOMContentLoaded', function(){
+  ReactDOM.render(<Person />, document.getElementById('app'));
+});
