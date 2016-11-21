8b8668f95cdb241b469f229113c5532566ee0ec4
Ray <raysmithdev@gmail.com>
2016-11-21 13:18:07 -0800

    Fixed rendering of App issue

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 require('babel-polyfill');
 
-var ReactDom = require('react-dom');
+// 1. You forgot to require React
+// 2. When you required ReactDOM you spelled ReactDOM AS ReactDom
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
 var Person = require('./components/person');
 
 document.addEventListener('DOMContentLoaded', function(){
